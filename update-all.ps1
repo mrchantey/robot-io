a067411c0b310190ba49ace66a004ed42bd17f21
Peter Hayman <peteyhayman@gmail.com>
2018-12-07 20:07:18 +1100

    patch

---
+++ update-all.ps1
@@ -0,0 +1,13 @@
+cd ./robot-io-client
+ncu -u
+npm update
+cd ../robot-io-johnny-five
+ncu -u
+npm update
+cd ../robot-io-serial
+ncu -u
+npm update
+cd ../robot-io-server
+ncu -u
+npm update
+cd ..
