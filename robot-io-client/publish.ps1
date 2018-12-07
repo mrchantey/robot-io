94e5d380baf009cdb34020b18a0402e8e4f6707b
Peter Hayman <peteyhayman@gmail.com>
2018-12-07 20:13:11 +1100

    patch

--- robot-io-client/publish.ps1
+++ robot-io-client/publish.ps1
@@ -1,6 +1,10 @@
 #do not call this script directly, will be called on npm publish
+echo 'building app..'
 npm run build
+echo 'fixing html paths..'
+node fixHtmlPaths.js
 npm version patch
+echo 'commiting to github..'
 cd ../
 git add .
 git commit -m 'patch'
